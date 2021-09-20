82f187c1027bc35167ffa250f072d5962ad109ee
Yeram522 <kyeram522@gmail.com>
2021-09-20 23:52:41 +0900

    기본적인 게임 화면 구현
    ✔ 수업시간에 배운 Screen 클래스와 Borland 헤더를 이용해, 지뢰찾기 필드를 만들었다.
    ✔ 콘솔 색 변경도 해보았다.

---
+++ Mine-Sweeper-game/Mine-Sweeper-game/Color.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Windows.h>
+#include <conio.h>
+#include <Windows.h>
+
+using namespace std;
+enum color4 {
+	Black = 0,
+	Blue,
+	Green,
+	Cyan,
+	Red,
+	Magenta,
+	Brown,
+	Lgray,
+	Dgray,
+	Lblue,
+	Lgreen,
+	Lcyan,
+	Lred,
+	Lmagenta,
+	Yellow,
+	White
+};
