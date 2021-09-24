8a529ba925ef0266368c018f1af6165edde2d07a
Yeram522 <kyeram522@gmail.com>
2021-09-25 01:11:38 +0900

    ✨header & cpp 파일 분할 및 정리
    key입력 코 +  Screen 클래스-> **Game2D 클래스**
    로 정리했다.
    main.cpp에 있는 Test Class는 Game2D 클래스를 상속받는다.

--- Mine-Sweeper-game/Mine-Sweeper-game/main.cpp
+++ Mine-Sweeper-game/Mine-Sweeper-game/main.cpp
@@ -2,221 +2,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
-#include "Screen.h"
+#include "Game2D.h"
 
-static HANDLE hStdin;
-static DWORD fdwSaveOldMode;
-
-static char blankChars[80];
-
-class TestGame 
+class TestGame :public Game2D
 {
 public:
 	Field* fields;
 	Position pos;
-	Screen screen;
+	//Screen screen;//Game2D로 합치고 부모 클래스가 됨. 부모클래스 생성자에서 초기화.
 	int flag_count;
 
 	TestGame()
-		:fields(new Field[11 * 10]), pos(0, 0), screen(10, 11), flag_count(0)
+		:Game2D(10,11),fields(new Field[11 * 10]), pos(0, 0),flag_count(0)
 	{
 		system("mode con cols=50 lines=25 | title Mine Swiper");
 		//flag_count = rand() % 11 + 10;//0~10-> (0+10)~(10+10) -> 10 ~20
-		flag_count = 5;//0~10-> (0+10)~(10+10) -> 10 ~20
+		flag_count = 5;//디버깅 때문에 5로 지정 해 놓음.
 		Field::Set_Field(11 * 10, flag_count, fields);
 		Field::Compute_Near_Mine(11 * 10, fields);
 	}
 
-	int run()
+	void update() override
 	{
-		DWORD cNumRead, fdwMode, i;
-		INPUT_RECORD irInBuf[128];
+		Game2D::Update_UI(this->flag_count);
 
+		draw(pos, fields);
 
-		memset(blankChars, ' ', 80);
-		blankChars[79] = '\0';
-		
-		// Get the standard input handle.
-
-		hStdin = GetStdHandle(STD_INPUT_HANDLE);
-		if (hStdin == INVALID_HANDLE_VALUE)
-			ErrorExit("GetStdHandle");
-		if (!GetConsoleMode(hStdin, &fdwSaveOldMode))
-			ErrorExit("GetConsoleMode");
-		/*
-			   Step-1:
-			   Disable 'Quick Edit Mode' option programmatically
-		 */
-		fdwMode = ENABLE_EXTENDED_FLAGS;
-		if (!SetConsoleMode(hStdin, fdwMode))
-			ErrorExit("SetConsoleMode");
-		/*
-		   Step-2:
-		   Enable the window and mouse input events,
-		   after you have already applied that 'ENABLE_EXTENDED_FLAGS'
-		   to disable 'Quick Edit Mode'
-		*/
-		fdwMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
-		if (!SetConsoleMode(hStdin, fdwMode))
-			ErrorExit("SetConsoleMode");
-		bool isLooping = true;
-		while (isLooping) {
-			Screen::Update_UI(this->flag_count);
-			
-
-			screen.clear();
-
-			if (GetNumberOfConsoleInputEvents(hStdin, &cNumRead)) {
-				Borland::gotoxy(0, 14);
-				printf("number of inputs %d\n", cNumRead);
-
-				if (cNumRead > 0) {
-
-					if (!ReadConsoleInput(
-						hStdin,      // input buffer handle
-						irInBuf,     // buffer to read into
-						128,         // size of read buffer
-						&cNumRead)) // number of records read
-						ErrorExit("ReadConsoleInput");
-					// Dispatch the events to the appropriate handler.
-
-					for (i = 0; i < cNumRead; i++)
-					{
-						switch (irInBuf[i].EventType)
-						{
-						case KEY_EVENT: // keyboard input
-							KeyEventProc(irInBuf[i].Event.KeyEvent);
-							break;
-
-						case MOUSE_EVENT: // mouse input
-							MouseEventProc(irInBuf[i].Event.MouseEvent);
-							break;
-
-						case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing
-							ResizeEventProc(irInBuf[i].Event.WindowBufferSizeEvent);
-							break;
-
-						case FOCUS_EVENT:  // disregard focus events
-
-						case MENU_EVENT:   // disregard menu events
-							break;
-
-						default:
-							ErrorExit("Unknown event type");
-							break;
-						}
-					}
-				}
-				Borland::gotoxy(0, 0);
-			}
-
-
-			screen.draw(pos, fields);
-
-			screen.render();
-
-
-			Sleep(100);
-
-		}
-		printf("\nGame Over\n");
-
-		return 0;
+		render();
 	}
-
-	void ErrorExit(const char* lpszMessage)
-	{
-		fprintf(stderr, "%s\n", lpszMessage);
-
-		// Restore input mode on exit.
-
-		SetConsoleMode(hStdin, fdwSaveOldMode);
-
-		ExitProcess(0);
-	}
-
-	void KeyEventProc(KEY_EVENT_RECORD ker)
-	{
-		Borland::gotoxy(0, 11);
-		printf("%s\r", blankChars);
-		switch (ker.wVirtualKeyCode) {
-		case VK_LBUTTON:
-			printf("left button ");
-			break;
-		case VK_BACK:
-			printf("back space");
-			break;
-		case VK_RETURN:
-			printf("enter key");
-			break;
-		case VK_LEFT:
-			printf("arrow left");
-			break;
-		case VK_UP:
-			printf("arrow up");
-			break;
-		default:
-			if (ker.wVirtualKeyCode >= 0x30 && ker.wVirtualKeyCode <= 0x39)
-				printf("Key event: %c ", ker.wVirtualKeyCode - 0x30 + '0');
-			else printf("Key event: %c ", ker.wVirtualKeyCode - 0x41 + 'A');
-			break;
-		}
-
-		Borland::gotoxy(0, 0);
-	}
-
-	void MouseEventProc(MOUSE_EVENT_RECORD mer)
-	{
-		Borland::gotoxy(0, 12);
-		printf("%s\r", blankChars);
-#ifndef MOUSE_HWHEELED
-#define MOUSE_HWHEELED 0x0008
-#endif
-		printf("Mouse event: ");
-
-		switch (mer.dwEventFlags)
-		{
-		case 0:
-			if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
-			{
-				printf("left button press %d %d\n", mer.dwMousePosition.X, mer.dwMousePosition.Y);
-			}
-			else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
-			{
-				printf("right button press \n");
-			}
-			else
-			{
-				printf("button press\n");
-			}
-			break;
-		case DOUBLE_CLICK:
-			printf("double click\n");
-			break;
-		case MOUSE_HWHEELED:
-			printf("horizontal mouse wheel\n");
-			break;
-		case MOUSE_MOVED:
-			printf("mouse moved %d %d\n", mer.dwMousePosition.X, mer.dwMousePosition.Y);
-			break;
-		case MOUSE_WHEELED:
-			printf("vertical mouse wheel\n");
-			break;
-		default:
-			printf("unknown\n");
-			break;
-		}
-		Borland::gotoxy(0, 0);
-	}
-
-	void ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
-	{
-		Borland::gotoxy(0, 13);
-		printf("%s\r", blankChars);
-		printf("Resize event: ");
-		printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
-		Borland::gotoxy(0, 0);
-	}
+	
 };
 
 int main()
