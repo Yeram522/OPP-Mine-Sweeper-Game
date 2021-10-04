81d26398e659985584263d79b5cd4f3822460cd8
Yeram522 <kyeram522@gmail.com>
2021-10-05 02:26:32 +0900

    싱글톤 패턴 적용
    마우스 입력이 인식 안되는 버그 있음.
    고칠 예정..ㅠ

--- Mine-Sweeper-game/Mine-Sweeper-game/Game2D.cpp
+++ Mine-Sweeper-game/Mine-Sweeper-game/Game2D.cpp
@@ -1,17 +1,10 @@
 #include "Game2D.h"
 //-----------이동완
-HANDLE Input::hStdin;
-DWORD Input::fdwSaveOldMode;
-char Input::blankChars[80];
-DWORD Input::cNumRead;
-DWORD Input::fdwMode;
-INPUT_RECORD Input::irInBuf[128];
-bool *isLooping;
+Input* Input::instance = nullptr;
+bool *isLooping;//ecter bool 선언.
 WindowPos Input:: ClickedPos;
 //-------------
-// 
-// 
-// 
+ 
 //맨 상단 윗줄의 UI를 업데이트 한다.
 void Game2D::Update_UI(const int _count)
 {
@@ -32,205 +25,15 @@ void Game2D::Update_UI(const int _count)
 	
 }
 
-void Input::Intialize()//Input Initialize
-{
-	memset(blankChars, ' ', 80);
-	blankChars[79] = '\0';
-
-	// Get the standard input handle.
-
-	hStdin = GetStdHandle(STD_INPUT_HANDLE);
-	if (hStdin == INVALID_HANDLE_VALUE)
-		ErrorExit("GetStdHandle");
-	if (!GetConsoleMode(hStdin, &fdwSaveOldMode))
-		ErrorExit("GetConsoleMode");
-	/*
-		   Step-1:
-		   Disable 'Quick Edit Mode' option programmatically
-	 */
-	fdwMode = ENABLE_EXTENDED_FLAGS;
-	if (!SetConsoleMode(hStdin, fdwMode))
-		ErrorExit("SetConsoleMode");
-	/*
-	   Step-2:
-	   Enable the window and mouse input events,
-	   after you have already applied that 'ENABLE_EXTENDED_FLAGS'
-	   to disable 'Quick Edit Mode'
-	*/
-	fdwMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
-	if (!SetConsoleMode(hStdin, fdwMode))
-		ErrorExit("SetConsoleMode");
-}
-
-void Input::ReadInput()
-{
-	if (!GetNumberOfConsoleInputEvents(hStdin, &cNumRead))
-	{
-		cNumRead = 0;
-		return;
-	}
-	Borland::gotoxy(0, 14);
-	printf("number of inputs %d\n", cNumRead);
-
-	if (cNumRead == 0) return;
-
-	if (!ReadConsoleInput(
-		hStdin,      // input buffer handle
-		irInBuf,     // buffer to read into
-		128,         // size of read buffer
-		&cNumRead)) // number of records read
-		ErrorExit("ReadConsoleInput");
-		// Dispatch the events to the appropriate handler.
-
-	for (int i = 0; i < cNumRead; i++)
-	{
-		switch (irInBuf[i].EventType)
-		{
-		case KEY_EVENT: // keyboard input
-			KeyEventProc(irInBuf[i].Event.KeyEvent);
-			break;
-
-		case MOUSE_EVENT: // mouse input
-			MouseEventProc(irInBuf[i].Event.MouseEvent);
-			break;
-
-		case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing
-			ResizeEventProc(irInBuf[i].Event.WindowBufferSizeEvent);
-			break;
-
-		case FOCUS_EVENT:  // disregard focus events
-
-		case MENU_EVENT:   // disregard menu events
-			break;
-
-		default:
-			ErrorExit("Unknown event type");
-			break;
-		}
-	}
-
-	Borland::gotoxy(0, 0);
-
-}
-
-bool Input::GetLeftMouseClick(DWORD eventflag)
-{
-	if (cNumRead == 0) return false;
-
-	for (int i = 0; i < cNumRead; i++)
-	{
-		if (irInBuf[i].EventType != MOUSE_EVENT) continue;
-
-		if (irInBuf[i].Event.MouseEvent.dwEventFlags == eventflag)
-			return true;
-	}
-}
-//마이크로소프트 제공 함수----------------------------------(약간 수정한 것도 있다.)
-void Input::ErrorExit(const char* lpszMessage)
-{
-	fprintf(stderr, "%s\n", lpszMessage);
-
-	// Restore input mode on exit.
-
-	SetConsoleMode(hStdin, fdwSaveOldMode);
-
-	ExitProcess(0);
-}
-
-void Input::KeyEventProc(KEY_EVENT_RECORD ker)
-{
-	Borland::gotoxy(0, 12);
-	printf("%s\r", blankChars);
-	switch (ker.wVirtualKeyCode) {
-	case VK_LBUTTON:
-		printf("left button ");
-		break;
-	case VK_BACK:
-		printf("back space");
-		break;
-	case VK_RETURN:
-		printf("enter key");
-		break;
-	case VK_ESCAPE:
-		printf("escape key");
-		*isLooping = false;
-		break;
-	case VK_UP:
-		printf("arrow up");
-		break;
-	default:
-		if (ker.wVirtualKeyCode >= 0x30 && ker.wVirtualKeyCode <= 0x39)
-			printf("Key event: %c ", ker.wVirtualKeyCode - 0x30 + '0');
-		else printf("Key event: %c ", ker.wVirtualKeyCode - 0x41 + 'A');
-		break;
-	}
-
-	Borland::gotoxy(0, 0);
-}
-
-void Input::MouseEventProc(MOUSE_EVENT_RECORD mer)
-{
-	Borland::gotoxy(0, 12);
-	printf("%s\r", blankChars);
-#ifndef MOUSE_HWHEELED
-#define MOUSE_HWHEELED 0x0008
-#endif
-	printf("Mouse event: ");
-
-	switch (mer.dwEventFlags)
-	{
-	case 0:
-		if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
-		{
-			printf("left button press %d %d\n", mer.dwMousePosition.X, mer.dwMousePosition.Y);
-			ClickedPos.x = mer.dwMousePosition.X;
-			ClickedPos.y = mer.dwMousePosition.Y;//전역변수에 입력받은 pos 넘겨줌.
-		}
-		else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
-		{
-			printf("right button press \n");
-		}
-		else
-		{
-			printf("button press\n");
-		}
-		break;
-	case DOUBLE_CLICK:
-		printf("double click\n");
-		break;
-	case MOUSE_HWHEELED:
-		printf("horizontal mouse wheel\n");
-		break;
-	case MOUSE_MOVED:
-		printf("mouse moved %d %d\n", mer.dwMousePosition.X, mer.dwMousePosition.Y);
-		break;
-	case MOUSE_WHEELED:
-		printf("vertical mouse wheel\n");
-		break;
-	default:
-		printf("unknown\n");
-		break;
-	}
-	Borland::gotoxy(0, 0);
-}
-
-void Input::ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
-{
-	Borland::gotoxy(0, 13);
-	printf("%s\r", blankChars);
-	printf("Resize event: ");
-	printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
-	Borland::gotoxy(0, 0);
-}
-//---------------------------------------------------------
-
 
 void Game2D::run()//자식게임클래스를 실행하는 함수이고 while문을 포함한다.
 {
 	bool isClear = false;
 	//키 입력 변수-------------
 	
-	Input::Intialize();
+	Input* Instance = Input::GetInstance();//Input instace 생성
+
+	//Input::Intialize();
 
 	//게임 루프 스타트!!
 	while (*isLooping) {
@@ -244,7 +47,7 @@ void Game2D::run()//자식게임클래스를 실행하는 함수이고 while문을 포함한다.
 
 		clear();
 
-		Input::ReadInput();
+		Instance->ReadInput();
 
 		update();//자식클래스에서 이 함수를 오버라이드 하여 사용한다!
 
--- Mine-Sweeper-game/Mine-Sweeper-game/Game2D.h
+++ Mine-Sweeper-game/Mine-Sweeper-game/Game2D.h
@@ -17,27 +17,217 @@ extern bool* isLooping;
 
 class Input
 {
-static HANDLE hStdin;
-static DWORD fdwSaveOldMode;
-static char blankChars[80];
-static DWORD cNumRead, fdwMode;
-static INPUT_RECORD irInBuf[128];
+private:
+	HANDLE hStdin;
+    DWORD fdwSaveOldMode;
+    char blankChars[80];
+    DWORD cNumRead,fdwMode;
+    INPUT_RECORD irInBuf[128];
+	static Input* instance; //1개의 static instace만 존재한다.
 
+	Input()
+	{
+		memset(blankChars, ' ', 80);
+		blankChars[79] = '\0';
 
+		// Get the standard input handle.
+
+		hStdin = GetStdHandle(STD_INPUT_HANDLE);
+		if (hStdin == INVALID_HANDLE_VALUE)
+			ErrorExit("GetStdHandle");
+		if (!GetConsoleMode(hStdin, &fdwSaveOldMode))
+			ErrorExit("GetConsoleMode");
+		/*
+			   Step-1:
+			   Disable 'Quick Edit Mode' option programmatically
+		 */
+		fdwMode = ENABLE_EXTENDED_FLAGS;
+		if (!SetConsoleMode(hStdin, fdwMode))
+			ErrorExit("SetConsoleMode");
+		/*
+		   Step-2:
+		   Enable the window and mouse input events,
+		   after you have already applied that 'ENABLE_EXTENDED_FLAGS'
+		   to disable 'Quick Edit Mode'
+		*/
+		fdwMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+		if (!SetConsoleMode(hStdin, fdwMode))
+			ErrorExit("SetConsoleMode");
+	}
+
+	~Input()
+	{
+		SetConsoleMode(hStdin, fdwSaveOldMode);
+	}
 
 public:
 	static WindowPos ClickedPos;
 	
-	//About KeyBoard and Mouse Input--------------------------------
-	static void ErrorExit(const char*);
-	static void Intialize();
-	static void ReadInput();
-	static void KeyEventProc(KEY_EVENT_RECORD);
-	static void MouseEventProc(MOUSE_EVENT_RECORD);
-	static void ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD);
-	//--------------------------------------------------------------
-	static bool GetLeftMouseClick(DWORD eventflag);
+	static Input* GetInstance()
+	{
+		if (instance == nullptr)
+		{
+			instance = new Input();//instace가 없다면 새로 만든다.
+		}
+		return instance;
+	}
 
+	void ReadInput()
+	{
+		if (!GetNumberOfConsoleInputEvents(hStdin, &cNumRead))
+		{
+			cNumRead = 0;
+			return;
+		}
+		Borland::gotoxy(0, 14);
+		printf("number of inputs %d\n", cNumRead);
+
+		if (cNumRead == 0) return;
+
+		if (!ReadConsoleInput(
+			hStdin,      // input buffer handle
+			irInBuf,     // buffer to read into
+			128,         // size of read buffer
+			&cNumRead)) // number of records read
+			ErrorExit("ReadConsoleInput");
+		// Dispatch the events to the appropriate handler.
+
+		for (int i = 0; i < cNumRead; i++)
+		{
+			switch (irInBuf[i].EventType)
+			{
+			case KEY_EVENT: // keyboard input
+				KeyEventProc(irInBuf[i].Event.KeyEvent);
+				break;
+
+			case MOUSE_EVENT: // mouse input
+				MouseEventProc(irInBuf[i].Event.MouseEvent);
+				break;
+
+			case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing
+				ResizeEventProc(irInBuf[i].Event.WindowBufferSizeEvent);
+				break;
+
+			default:
+				ErrorExit("Unknown event type");
+				break;
+			}
+		}
+
+		Borland::gotoxy(0, 0);
+
+	}
+
+	void ErrorExit(const char* lpszMessage)
+	{
+		fprintf(stderr, "%s\n", lpszMessage);
+
+		// Restore input mode on exit.
+
+		SetConsoleMode(hStdin, fdwSaveOldMode);
+
+		ExitProcess(0);
+	}
+
+	void KeyEventProc(KEY_EVENT_RECORD ker)
+	{
+		Borland::gotoxy(0, 12);
+		printf("%s\r", blankChars);
+		switch (ker.wVirtualKeyCode) {
+		case VK_LBUTTON:
+			printf("left button ");
+			break;
+		case VK_BACK:
+			printf("back space");
+			break;
+		case VK_RETURN:
+			printf("enter key");
+			break;
+		case VK_ESCAPE:
+			printf("escape key");
+			*isLooping = false;
+			break;
+		case VK_UP:
+			printf("arrow up");
+			break;
+		default:
+			if (ker.wVirtualKeyCode >= 0x30 && ker.wVirtualKeyCode <= 0x39)
+				printf("Key event: %c ", ker.wVirtualKeyCode - 0x30 + '0');
+			else printf("Key event: %c ", ker.wVirtualKeyCode - 0x41 + 'A');
+			break;
+		}
+
+		Borland::gotoxy(0, 0);
+	}
+
+	void MouseEventProc(MOUSE_EVENT_RECORD mer)
+	{
+		Borland::gotoxy(0, 12);
+		printf("%s\r", blankChars);
+#ifndef MOUSE_HWHEELED
+#define MOUSE_HWHEELED 0x0008
+#endif
+		printf("Mouse event: ");
+
+		switch (mer.dwEventFlags)
+		{
+		case 0:
+			if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+			{
+				printf("left button press %d %d\n", mer.dwMousePosition.X, mer.dwMousePosition.Y);
+				ClickedPos.x = mer.dwMousePosition.X;
+				ClickedPos.y = mer.dwMousePosition.Y;//전역변수에 입력받은 pos 넘겨줌.
+			}
+			else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
+			{
+				printf("right button press \n");
+			}
+			else
+			{
+				printf("button press\n");
+			}
+			break;
+		case DOUBLE_CLICK:
+			printf("double click\n");
+			break;
+		case MOUSE_HWHEELED:
+			printf("horizontal mouse wheel\n");
+			break;
+		case MOUSE_MOVED:
+			printf("mouse moved %d %d\n", mer.dwMousePosition.X, mer.dwMousePosition.Y);
+			break;
+		case MOUSE_WHEELED:
+			printf("vertical mouse wheel\n");
+			break;
+		default:
+			printf("unknown\n");
+			break;
+		}
+		Borland::gotoxy(0, 0);
+	}
+
+	void ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
+	{
+		Borland::gotoxy(0, 13);
+		printf("%s\r", blankChars);
+		printf("Resize event: ");
+		printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
+		Borland::gotoxy(0, 0);
+	}
+
+	bool GetLeftMouseClick(DWORD eventflag)
+	{
+		if (cNumRead == 0) return false;
+
+		for (int i = 0; i < cNumRead; i++)
+		{
+			if (irInBuf[i].EventType != MOUSE_EVENT) continue;
+
+			if (irInBuf[i].Event.MouseEvent.dwEventFlags == eventflag)
+				return true;
+		}
+	}
+	
 	
 };
 
@@ -48,9 +238,9 @@ private:
 	int height;
 	int size;
 	char* canvas;
-
-public:
 	
+
+public:	
 	static bool IsMouseClicked;
 
 	Game2D(int width = 10, int height = 10)
